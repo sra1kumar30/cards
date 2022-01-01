a2431d31291b48584b9207887ad7688efc393b65
sra1kumar30 <sra1kumar30@gmail.com>
2022-01-01 21:05:10 +0100

    cards app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cards';
+  posts = [
+    {
+      title: 'This is a super bike',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biker',
+      content: 'Checkout out this cool thing!',
+    },
+    {
+      title: 'This is a best tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'naturelover',
+      content: 'One of the best tree pics',
+    },
+    {
+      title: 'Guess the mountain name!',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainclimber',
+      content: 'I have already climbed this mountain!',
+    },
+    {
+      title: 'Guess the mountain name!',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainclimber',
+      content: 'I have already climbed this mountain!',
+    },
+  ];
 }
